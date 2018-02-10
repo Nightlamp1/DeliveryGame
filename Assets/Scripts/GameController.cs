49f14c6b3978437318aec155f71e551ea8187cc5
Nightlamp1 <robert.britt2011@gmail.com>
2018-02-10 16:17:07 -0600

    Basic UI elements added for Score/Health

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     public static int playerScore = 0;
+    public Text playerScoreText;
 
     public static GameObject[] intersectionObjects;
     private Vector3[] intersections;
@@ -16,6 +18,6 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        playerScoreText.text = "Player Score : " + playerScore;
 	}
 }
